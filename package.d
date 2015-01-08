ae3485a964566de656cb692a840b0f96275a6244
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-01-08 22:27:08 +0200

    changes to use 4 spaces as indent

--- package.d
+++ package.d
@@ -1,9 +1,9 @@
 module minilib;
 /**
-  This file is part of the "minilib"
+    This file is part of the "minilib"
 
-  @license   The MIT License (MIT) Included in this distribution
-  @author    Zaher Dirkey <zaher at yahoo dot com>
+    @license   The MIT License (MIT) Included in this distribution
+    @author    Zaher Dirkey <zaher at yahoo dot com>
 */
 
 public import minilib.sets;
