c3cc0af88e41e1c0c09607d703925d0c27141dfb
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-01-20 20:43:27 +0200

    change tab spaces

--- metaclasses.d
+++ metaclasses.d
@@ -7,20 +7,20 @@ module minilib.metaclasses;
 */
 
 /**
-    @describe:
-        Manage creating objects from variable classes
+@describe:
+    Manage creating objects from variable classes
     @ref:
     @example:
-        alias MyMetaClass = MetaClass!BaseClass mc;
+    alias MyMetaClass = MetaClass!BaseClass mc;
 
-    @doc:  
-        check if is instance of my class
-        typeid(obj1) == typeid(obj2)
-        typeid(obj1) == typeid(MyClass)
-        
-    @hints:
-        if you want to use factory your class must have default constructor (or no constructors).  
-        if you have subclass constructor SubClass(a,b) you need to add default constructor even if u have it in the base class, D hide the functions in base class if u add new one in subclass.
+@doc:  
+    check if is instance of my class
+    typeid(obj1) == typeid(obj2)
+    typeid(obj1) == typeid(MyClass)
+
+@hints:
+    if you want to use factory your class must have default constructor (or no constructors).  
+    if you have subclass constructor SubClass(a,b) you need to add default constructor even if u have it in the base class, D hide the functions in base class if u add new one in subclass.
 */
 
 
@@ -28,7 +28,7 @@ pragma(msg, "You are using metaclasses.d it is not finished!");
 
 /*
     use hasMember to check, http://dlang.org/phobos/std_traits.html
-        template hasMember(T, string name)
+    template hasMember(T, string name)
 */
 struct MetaClass(T)  
 {
