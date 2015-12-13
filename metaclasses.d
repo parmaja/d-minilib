928bccf7080de414111c36236a07881b7e7e19cf
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-12-13 07:17:51 +0200

    Fixed my email address

--- metaclasses.d
+++ metaclasses.d
@@ -3,7 +3,7 @@ module minilib.metaclasses;
     This file is part of the "minilib"
 
     @license   The MIT License (MIT) Included in this distribution
-    @author    Zaher Dirkey <zaher at yahoo dot com>
+    @author    Zaher Dirkey <zaherdirkey at yahoo dot com>
 */
 
 /**
